4680acd2ded33f239b53e22794f4067f908a3c60
yousef arif <yousefarifagha@gmail.com>
2021-11-11 16:29:43 +0000

    models & validations

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :movie, presence: true, uniqueness: { scope: :list }
+  validates :comment, length: { minimum: 6 }
+end
